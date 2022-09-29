eb673d4874e2ff9defa4d5afe0102b0c7e004b33
Damasceno <eng.damasceno@alu.ufc.br>
2022-09-29 14:31:13 -0300

    Add files via upload

--- AGENTE DE RESOLUÇÃO DE PROBLEMAS/main.py
+++ AGENTE DE RESOLUÇÃO DE PROBLEMAS/main.py
@@ -11,40 +11,40 @@ def bfs(startingNode, destinationNode):
   # Para acompanhar o que visitamos
   explorados = {}
   # manter o controle de distância
-  distance = {}
-  # nó pai do gráfico específico
-  parent = {}
+  distancia = {}
+  # vai armazenar todas as cidades e marcar com true os vizinhos
+  vizinhos = {}
 
   bfs_traversal_output = []
   # O BFS é baseado em fila, portanto, usando 'Queue' do python embutido
   borda = Queue()
 
-  # viajando pelas cidades no mapa
-  for city in mapaDaRomania.grafo.keys():
+  # viajando pelas cidades no mapa e setando
+  for cidade in mapaDaRomania.grafo.keys():
     # uma vez que inicialmente nenhuma cidade é visitada, então não haverá nada na lista visitada
-    explorados[city] = False
-    parent[city] = None
+    explorados[cidade] = False
+    vizinhos[cidade] = None
     # setando as distancias em -1
-    distance[city] = -1
+    distancia[cidade] = -1
 
   # Começando
   startingCity = startingNode
-  # Já foi explorado
+  # Já foi explorado, pois começamos nela
   explorados[startingCity] = True
   # setando a distancia da cidade origem
-  distance[startingCity] = 0
+  distancia[startingCity] = 0
   borda.put(startingCity)
 
   while not borda.empty():
-    u = borda.get()  # ppegando rimeiro elemento da fila
+    u = borda.get()  # pegando primeiro elemento da fila
     bfs_traversal_output.append(u)
 
     # explore as transições que saem do estado u
     for v in mapaDaRomania.grafo[u]:
       if not explorados[v]:
         explorados[v] = True
-        parent[v] = u
-        distance[v] = distance[u] + 1
+        vizinhos[v] = u
+        distancia[v] = distancia[u] + 1
         borda.put(v)
 
   # chegar à nossa cidade de destino,
@@ -53,7 +53,7 @@ def bfs(startingNode, destinationNode):
   while g is not None:
     # adicionando cidade na rota
     rota.append(g)
-    g = parent[g]
+    g = vizinhos[g]
 
   rota.reverse()
   # imprimindo o caminho para nossa cidade de destino
